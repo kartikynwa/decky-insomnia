6fc24cd372fa5dc1a36b3fa8527ae8a73d284db0
Kartik Singh <kartik.ynwa@gmail.com>
2025-11-02 20:46:04 +0530

    mpv_script: Register events only in game mode

--- mpv_script/dont_sleep.lua
+++ mpv_script/dont_sleep.lua
@@ -1,11 +1,13 @@
 local function send_message(message)
     local file = io.open("/tmp/decky_insomnia_fifo", "a")
     if file then
-        file:write(message)
+        file:write(message .. "\n")
         file:flush()
         file:close()
     end
 end
 
-mp.register_event("start-file", function() send_message("inhibit\n") end)
-mp.register_event("end-file", function() send_message("restore\n") end)
+if os.getenv("SteamDeck") == "1" then
+    mp.register_event("start-file", function() send_message("inhibit") end)
+    mp.register_event("end-file", function() send_message("restore") end)
+end
